ab3d8f849372c5915827f9da39c7dd7b5f97b32f
sergiodiazp45 <sergio.diazp45@gmail.com>
2013-09-18 08:53:29 -0500

    primer ejemplo en javac

---
+++ Ejemplo3/src/CRelacion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class CRelacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num = 23;
+        
+        if (num %2 ==0 ){
+            
+            System.out.println("Numero par");
+        }
+        
+        else{
+            
+            System.out.println("Numero impar");
+        }
+            
+    }
+}
